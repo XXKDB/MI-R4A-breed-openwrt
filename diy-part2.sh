3707fefb20c88d38cfd6e3cef3cfddb0d6de97c2
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-05-29 16:16:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,6 +29,7 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-theme-bootstrap
 rm -rf package/lean/luci-theme-material
 rm -rf package/lean/luci-theme-netgear
+rm -rf package/lean/luci-theme-ifit
 
 #下载主题luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
