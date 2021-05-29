acd351b5519efebe31ff5aa07d3342fad1b745f4
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-05-29 15:12:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -82,6 +82,8 @@ sed -i 's/#9effff57/#9FC4D557/g' package/lean/luci-theme-argon_armygreen/htdocs/
 
 #主机名右上角符号❤
 #sed -i 's/<a class="brand" href="#"><%=boardinfo.hostname or "?"%> ❤</a>/<a class="brand" href="#"><%=boardinfo.hostname or "?"%> ❤</a>/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/header.htm
+#登陆页面右下角技术支持  跳转网站“https://github.com/openwrt/luci”   名称“可爱动漫主题”
+#sed -i 's/<a href="https://github.com/openwrt/luci">可爱动漫主题 <%= ver.luciname %> (<%= ver.luciversion %>)</a>/<a href="https://github.com/openwrt/luci">可爱动漫主题 <%= ver.luciname %> (<%= ver.luciversion %>)</a>/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/footer.htm
 
 
 #已选中
