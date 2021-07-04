1aadf5d51192d63c303157494f51fa9a1c7bd884
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-07-04 23:18:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,41 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 取消插件注释
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# 添加插件源码
+# sed -i '$a src-git ddnsto https://github.com/linkease/ddnsto-openwrt' feeds.conf.default
+# sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
+# sed -i '$a src-git dnsfilter https://github.com/garypang13/luci-app-dnsfilter' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+
+# 添加插件源码
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# passwall依赖
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+
+### 修改为R4A千兆版Breed直刷版
+# 1.修改 mt7621_xiaomi_mir3g-v2.dts
+export shanchu1=$(grep  -a -n -e '&spi0 {' openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts|cut -d ":" -f 1)
+export shanchu2=$(grep  -a -n -e '&pcie {' openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts|cut -d ":" -f 1)
+export shanchu2=$(expr $shanchu2 - 1)
+export shanchu2=$(echo $shanchu2"d")
+sed -i $shanchu1,$shanchu2 openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
+#sed -i '$49,112d' openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
+grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' openwrt/target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
+echo "" >> youhua.txt
+echo "" >> youhua.txt
+export shanchu1=$(expr $shanchu1 - 1)
+export shanchu1=$(echo $shanchu1"r")
+sed -i "$shanchu1 youhua.txt" openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
+#sed -i '48r youhua.txt' openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
+rm -rf youhua.txt
+# 2.修改mt7621.mk
+export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' openwrt/target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
+export imsize1=$(expr $imsize1 + 2)
+export imsize1=$(echo $imsize1"s")
+sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" openwrt/target/linux/ramips/image/mt7621.mk
