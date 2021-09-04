b3fd153a814171913e539c4ad8071a73fa1bfdf4
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-09-04 14:12:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,25 +26,24 @@
 # passwall依赖
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-
 ### 修改为R4A千兆版Breed直刷版
-# 1.修改 mt7621_xiaomi_mir3g-v2.dts
-export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts|cut -d ":" -f 1)
-export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts|cut -d ":" -f 1)
+## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
+## 1.修改 mt7621_xiaomi_mir3g-v2.dts
+export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
+export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
 export shanchu2=$(expr $shanchu2 - 1)
 export shanchu2=$(echo $shanchu2"d")
-sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
-#sed -i '$49,112d' openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
+sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
 grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
 echo "" >> youhua.txt
 echo "" >> youhua.txt
 export shanchu1=$(expr $shanchu1 - 1)
 export shanchu1=$(echo $shanchu1"r")
-sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
-#sed -i '48r youhua.txt' openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
+sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
 rm -rf youhua.txt
-# 2.修改mt7621.mk
+## 2.修改mt7621.mk
 export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
 export imsize1=$(expr $imsize1 + 2)
 export imsize1=$(echo $imsize1"s")
 sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt7621.mk
+
