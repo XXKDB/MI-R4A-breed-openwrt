e235da1977f9915ef33f2b02881b1a4d012ef3d3
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-10-11 12:44:24 +0800

    Update R4A.sh

--- MI-R4A/R4A.sh
+++ MI-R4A/R4A.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #rm -rf package/lean/luci-theme-argon
 # 修改openwrt登陆地址,把下面的192.168.31.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把YOU-R4A修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='MIWIFI'' package/lean/default-settings/files/zzz-default-settings
