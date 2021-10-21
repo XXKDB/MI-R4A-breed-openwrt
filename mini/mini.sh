6451fd0c2557e35653da70aeff67a408bcce5a53
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-10-21 21:20:54 +0800

    Update mini.sh

--- mini/mini.sh
+++ mini/mini.sh
@@ -19,9 +19,9 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='XXKDB'' package
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 #sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/OpenWrt /星新课代表/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 星新课代表 /g" package/lean/default-settings/files/zzz-default-settings
 #更改主机型号，支持中文。 
-sed -i "s/Xiaomi MiWiFi Mini/星新课代表制作出品/g" target/linux/ramips/dts/mt7620a_xiaomi_miwifi-mini.dts
+sed -i "s/Xiaomi MiWiFi Mini/星 新 课 代 表 制 作 出 品/g" target/linux/ramips/dts/mt7620a_xiaomi_miwifi-mini.dts
 
 #删除原默认主题
 rm -rf package/lean/luci-theme-argon
@@ -34,7 +34,7 @@ rm -rf package/lean/luci-theme-ifit
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
 git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
-#git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-ifit
+git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-ifit
 
 # 修改luci-theme-argon_armygreen主题渐变色，16进制RGB
 #登录页面背景颜色 透明值
