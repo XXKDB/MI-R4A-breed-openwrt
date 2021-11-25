562f4720b91c32144e42bd484f3a860402e4ea8f
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-11-26 01:16:26 +0800

    Update mini.sh

--- mini/mini.sh
+++ mini/mini.sh
@@ -15,13 +15,13 @@
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把YOU-R4A修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='XXKDB'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='MI-mini'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 #sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 星新课代表 /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /编译时间 $(TZ=UTC-8 date "+%Y.%m.%d") @ 星新课代表 /g" package/lean/default-settings/files/zzz-default-settings
 #更改主机型号，支持中文。 
-sed -i "s/Xiaomi MiWiFi Mini/星 新 课 代 表 制 作 出 品/g" target/linux/ramips/dts/mt7620a_xiaomi_miwifi-mini.dts
+sed -i "s/Xiaomi MiWiFi Mini/小米mini路由/g" target/linux/ramips/dts/mt7620a_xiaomi_miwifi-mini.dts
 
 sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
 
