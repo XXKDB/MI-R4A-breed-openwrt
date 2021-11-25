7bdfe413a65b147eb3137a1052c10095cd5e6c1c
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-11-26 00:54:51 +0800

    Update CR660x.sh

--- MI-CR660x/CR660x.sh
+++ MI-CR660x/CR660x.sh
@@ -23,9 +23,17 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='XXKDB'' package
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 #sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 sed -i "s/OpenWrt /星新课代表/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/Xiaomi Mi Router CR660x/星新课代表制作出品/g" target/linux/ramips/dts/mt7621_xiaomi_mi-router-cr660x.dts
 
+sed -i "s/Xiaomi Mi Router CR660x/小米CR660X路由器/g" target/linux/ramips/dts/mt7621_xiaomi_mi-router-cr660x.dts
 
+# 状态系统增加个性信息
+sed -i "s/exit 0//" package/lean/default-settings/files/zzz-default-settings
+echo 'sed -i "/CPU usage/a\<tr><td width="33%">关于</td><td><a class="author-blog" href="https://myxiaochuang.gitee.io">作者博客</a>&nbsp;&nbsp;&nbsp;<a class="author-blog" href="https://space.bilibili.com/359789531">哔哩哔哩</a></td></tr>" /usr/lib/lua/luci/view/admin_status/index.htm' >> package/lean/default-settings/files/zzz-default-settings
+echo 'sed -i "/作者博客/a\<tr><td width="33%">编译源地址</td><td><a class="author-blog" href="https://github.com/YL2209/MI-R4A-breed-openwrt">编译源地址</a></td></tr>" /usr/lib/lua/luci/view/admin_status/index.htm' >> package/lean/default-settings/files/zzz-default-settings
+echo 'sed -i "/编译源地址/a\<tr><td width="33%">表情包</td><td><img class="emoji-icon" src="https://cdn.jsdelivr.net/gh/XXKDB/img_cdn/img/beian2.gif"></td></tr>" /usr/lib/lua/luci/view/admin_status/index.htm' >> package/lean/default-settings/files/zzz-default-settings
+echo "" >> package/lean/default-settings/files/zzz-default-settings
+echo "" >> package/lean/default-settings/files/zzz-default-settings
+echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
 
 #删除原默认主题
 rm -rf package/lean/luci-theme-argon
