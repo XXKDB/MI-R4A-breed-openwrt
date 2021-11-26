bdc8dec58d4e33080118d51d5c8c9fc2a2559073
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-11-27 00:51:26 +0800

    Create diy-part3.sh

---
+++ MI-CR660x/diy-part3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 3 (Before Update feeds)
+#
