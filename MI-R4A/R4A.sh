01a99f88baf39a4c56b92adf3c4490acabdc6625
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-11-28 11:54:34 +0800

    Update R4A.sh

--- MI-R4A/R4A.sh
+++ MI-R4A/R4A.sh
@@ -54,7 +54,7 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-theme-bootstrap
 rm -rf package/lean/luci-theme-material
 rm -rf package/lean/luci-theme-netgear
-rm -rf package/lean/luci-theme-ifit
+rm -rf package/kenzo/luci-theme-ifit
 
 #下载主题luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
